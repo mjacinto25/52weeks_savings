d911cfcf3d328d8e2215511d25c2f90af03fd69b
Melanie Jacinto <Melanie Jacinto>
2017-01-12 17:14:10 +0800

    yes/no button stuff

--- index.php
+++ index.php
@@ -1,10 +1,56 @@
 <html>
+<head>Savings Template</head>
 <body>
 
-	<form action = "savings_view.php" method = "post">
+	<form action = "index.php" method = "post">
 	Set Base Amount: <input type="number" name = "base_amount">
 	<input type = "submit">
 	</form>
+
+	<table border = "1">
+	<tr>
+	<td>Weeks</td>
+	<td>Reference Amount</td>
+	<td>Remittance Amount</td>
+	<td>Action</td>
+	</tr> 
+
+
+	<?php
+
+	if (isset($_POST ["base_amount"])) {
+		$base_amount = $_POST ["base_amount"];
+		$reference_amount = $base_amount; 
+		
+		for ($i = 1 ; $i <= 52; $i++){
+			
+	?>
+	
+			<tr>
+				<td><?php echo $i;?></td>
+				<td><?php echo $reference_amount; ?></td>
+				<td><input type ="text" name = "remittance_amount" disabled></td>
+				<td><input type ="submit" value = "SAVE"></td>
+			</tr>
+			<?php
+			$reference_amount = $reference_amount + $base_amount;
+
+			
+		}
+			?>
+<form action = "" method = "post">
+           Do you want to save this template? 
+           <input type = "submit" value = "yes">
+           <input type = "submit" value = "no">
+          </form>
+	<?php
+	}
+	?>	
+		
+	
+	
+	</table>
+
 
 </body>
 </html>
--- savings_view.php
+++ savings_view.php
@@ -1,39 +1,3 @@
-<html>
-<body>
-
-<table border = "1">
-	<tr>
-	<td>Weeks</td>
-	<td>Reference Amount</td>
-	<td>Remittance Amount</td>
-	<td>Action</td>
-	</tr> 
-
-<?php
-if (isset($_POST ["base_amount"])) {
-	$base_amount = $_POST ["base_amount"];
-	$reference_amount = $base_amount; 
-	for ($i = 1 ; $i <= 52; $i++){
-?>
-
-	<tr>
-		<td><?php echo $i;?></td>
-		<td><?php echo $reference_amount; ?></td>
-		<td><input type ="text" name = "remittance_amount"></td>
-		<td><input type ="submit" value = "SAVE"></td>
-
-	</tr>
-
-	<?php
-	$reference_amount = $reference_amount + $base_amount;
-	}
-}
-    ?>
-</table>
-
-</body>
-</html>
-
 <?php 
 $servername = "localhost";
 $username = "root";
@@ -51,24 +15,28 @@ if (isset($_POST ["base_amount"])) {
 	}
 	
 	//Select functionality
-	$sql = "SELECT amount from base_amount";
+	$sql = "SELECT base_amount from savings";
 	$result = $conn->query($sql);
-
+/*
 	if ($result->num_rows > 0){
 	
 	//Update functionality
-	$sql = "UPDATE base_amount SET amount = (".$base_amount.")";
+	$sql = "UPDATE savings SET base_amount = (".$base_amount.")";
 	$result = $conn->query($sql);
 
 	}else {
 	
 	//Insert functionlaity
-	$sql = "INSERT INTO base_amount (amount) VALUES (".$base_amount.")";
+	$sql = "INSERT INTO savings (base_amount) VALUES (".$base_amount.")";
 	$result = $conn->query($sql);
-	}
+	}*/
 	
 	mysqli_close($conn);
-}
+}*/
+
+
+
+
 ?> 
 
 
